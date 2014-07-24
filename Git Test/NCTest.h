4c2a7c54dfbf71bac7266127397799d78b260b4e
Colin Guilfoyle <colinguilfoyle@Colins-MacBook-Pro.local>
2014-07-24 15:52:51 +0100

    added a test class

---
+++ Git Test/NCTest.h
@@ -0,0 +1,13 @@
+//
+//  NCTest.h
+//  Git Test
+//
+//  Created by Colin Guilfoyle on 24/07/2014.
+//  Copyright (c) 2014 Nebula Consultancy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NCTest : NSObject
+
+@end
---
+++ Git Test/NCTest.m
@@ -0,0 +1,13 @@
+//
+//  NCTest.m
+//  Git Test
+//
+//  Created by Colin Guilfoyle on 24/07/2014.
+//  Copyright (c) 2014 Nebula Consultancy. All rights reserved.
+//
+
+#import "NCTest.h"
+
+@implementation NCTest
+
+@end
